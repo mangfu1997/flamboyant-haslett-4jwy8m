acbadc3b25c170f835df78bf2fbf45af0f5ea207
mangfu1997 <wdh3876@gmail.com>
2025-11-10 21:01:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "24px",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      测试！✨
+    </div>
+  );
+}
+
+export default App;
